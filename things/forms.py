a33b5b958423e1c4b97ba02a24d93720d1ba868e
Hui Teo <hui.teo@kcl.ac.uk>
2023-10-18 17:10:19 +0100

    add things form

--- things/forms.py
+++ things/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from things.models import Thing
 
-class ThingsForm(forms.ModelForm):
+class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ["name", "description", "quantity"]
